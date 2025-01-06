3f2625b44a7ef8d50639567878c604c9715225af
CigdemOzel <cigdemozel17@gmail.com>
2025-01-06 19:54:01 +0300

    Update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,20 +8,11 @@ public class Main {
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
         if(clock<0 || clock>23) {
             return false;
-        } else if (isBarking && (clock < 8 || clock >= 20)) {
-            return true;
-        } else {
-            return false;
-        }
+        } else return isBarking && (clock < 8 || clock >= 20);
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       if(firstAge>=13 && firstAge<=19 || secondAge>=13 && secondAge<=19 || thirdAge>=13 && thirdAge<=19) {
-           return true;
-
-       } else {
-           return false;
-       }
+        return firstAge >= 13 && firstAge <= 19 || secondAge >= 13 && secondAge <= 19 || thirdAge >= 13 && thirdAge <= 19;
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
